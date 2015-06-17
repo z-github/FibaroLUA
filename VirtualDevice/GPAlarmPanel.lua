b2990fc8438fc9f286860248e59cdc9f4878ebfb
gianpietrospagna <gianpietrospagna@gmail.com>
2015-06-17 10:27:40 +0200

    test

--- VirtualDevice/GPAlarmPanel.lua
+++ VirtualDevice/GPAlarmPanel.lua
@@ -1,9 +1,9 @@
 if  ( fibaro:getGlobalValue("CentralinaStatoArm") == "Armata" )
 then
-  fibaro:call(171, "setProperty", "ui.LabelHC2StatoArm.value", "pippo")
+  fibaro:call(171, "setProperty", "ui.LabelHC2StatoArm.value", "pippoooo")
 end
 
 if  ( fibaro:getGlobalValue("CentralinaStatoArm") == "NON Armata" )
 then
-  fibaro:call(171, "setProperty", "ui.LabelHC2StatoArm.value", "pluto")
+  fibaro:call(171, "setProperty", "ui.LabelHC2StatoArm.value", "plutooooo")
 end
